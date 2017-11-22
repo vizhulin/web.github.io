7f021c9c0212fc6a6eb0740e16cc988b0d31bc37
vizhulin <vizhulin1@yandex.ru>
2017-11-22 23:58:54 +0300

    Update app.js

--- task3/app.js
+++ task3/app.js
@@ -1,28 +1,23 @@
-function add(text) {
-    var li = document.createElement('li');
-    var span = document.createElement('span');
-    span.innerHTML = text;
-    li.appendChild(span);
-    var button1 = document.createElement("button");
-    button1.innerHTML = "Удалить";
-    button1.addEventListener("click",  function(){ li.remove()});
-
-    li.appendChild(button1);
-    ul.appendChild(li);
-}
-
-var div = document.getElementById("root");
-var ul = document.createElement('ul');
-div.appendChild(ul);
-
-add("Сделать задание #3 по web-программированию");
-
-var input = document.createElement("input");
-input.id = "add_task_input";
-
-var button2 = document.createElement("button");
-button2.innerHTML = "add_task";
-button2.id = "add_task";
-button2.addEventListener("click", function(){add(input.value)});
-div.appendChild(input);
-div.appendChild(button2);
+$(document).ready(function()
+{
+	$("#root").append("<ul><li></li></ul>");
+	$("li").append("<span>Сделать задание #3 по web-программированию</span>");
+	$("li").append("<button>Удалить</button>").click(function()
+	{
+		$(this).remove();
+	});
+	$("#root").append("<input id='add_task_input'>");
+	$("#root").append("<button id='add_task'></button>");
+	var i = 0;
+	$("#add_task").click(function()
+	{
+		++i;
+		var newstr = $("input").val();
+		$("ul").append($("<li><span></span></li>").attr("id", i));
+		$("#" + i).text(newstr);
+		$("#" + i).append("<button>Удалить</button>").click(function()
+		{
+			$(this).remove();
+		})
+	});
+});
